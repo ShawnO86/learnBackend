7e3ee5496a8d4bad4df1c27b2004e9b02e43211e
Shawn <99354351+ShawnO86@users.noreply.github.com>
2023-08-17 10:41:45 -0500

    patch (update) one

--- first_mongoDB_RestAPI/server/routes/blogPosts.js
+++ first_mongoDB_RestAPI/server/routes/blogPosts.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import blogItem from '../models/blogPost.js'
+import blogItem from '../models/blogPost.js';
 
 //create one
 router.post('/', async (req, res) => {
@@ -14,7 +14,7 @@ router.post('/', async (req, res) => {
         const newPost = await post.save();
         res.status(201).json(newPost);
     } catch (e) {
-        res.status(400).json({ message: e.message })
+        res.status(400).json({ message: e.message });
     }
 });
 
@@ -23,7 +23,7 @@ router.get('/', async (req, res) => {
     try {
         const posts = await blogItem.find();
         //for listing of all blog posts in cards:
-        let postArr = []
+        let postArr = [];
         posts.forEach((post) => {
             postArr.push({
                 id: post.id,
@@ -32,7 +32,7 @@ router.get('/', async (req, res) => {
                 shortDesc: post.shortDesc,
                 date: post.dateEntered
             })
-        })
+        });
         res.json(postArr);
     } catch (e) {
         res.status(500).json({ message: e.message });
@@ -45,25 +45,51 @@ router.get('/:id', getBlog, (req, res) => {
 });
 
 //update one
-router.patch('/:id', getBlog, (req, res) => {
-
-})
+router.patch('/:id', getBlog, async (req, res) => {
+    if (req.body.title != null) {
+        res.selectedBlog.title = req.body.title
+    }
+    if (req.body.postType != null) {
+        res.selectedBlog.postType = req.body.postType
+    }
+    if (req.body.content != null) {
+        res.selectedBlog.content = req.body.content
+    }
+    if (req.body.shortDesc != null) {
+        res.selectedBlog.shortDesc = req.body.shortDesc
+    }
+    res.selectedBlog.dateEntered = Date.now();
+    try {
+        const updatedPost = await res.selectedBlog.save();
+        res.json({
+            Message: "Updated Post",
+            Updatedpost: updatedPost
+        });
+    } catch (e) {
+        res.status(400).json({ message: e.message });
+    }
+});
 
 //delete one
-router.post('/:id', getBlog, (req, res) => {
-
+router.post('/:id', getBlog, async (req, res) => {
+    try {
+        await res.selectedBlog.deleteOne();
+        res.json({ message: "Post deleted." });
+    } catch (e) {
+        res.status(500).json({ message: e.message });
+    }
 })
 
 async function getBlog(req, res, next) {
     let blog;
     try {
-        blog = await blogItem.findById(req.params.id)
+        blog = await blogItem.findById(req.params.id);
         if (blog == null) {
-            return res.status(404).json({ message: "Cannot find subscriber" });
+            return res.status(404).json({ message: "Cannot find post." });
         }
     } catch (e) {
         if (blog == undefined) {
-            return res.status(404).json({ message: "Cannot find subscriber" });
+            return res.status(404).json({ message: "Cannot find post." });
         }
         return res.status(500).json({ message: e.message });
     }
@@ -71,4 +97,4 @@ async function getBlog(req, res, next) {
     next();
 }
 
-export default router
+export default router;
